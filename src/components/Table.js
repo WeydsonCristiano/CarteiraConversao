7cbfa0e530fb500ed12bd5b51907c81f9fdebdcc
Cristiano <weydsoncristiano@hotmail.com>
2022-10-04 10:43:44 -0300

    resolução req7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
+  // botãoRemover = (index) => {
+  //   const itensRemover = this.states.itens.slice();
+  //   itensRemover.splice(index, 1);
+  //   this.setState({ itens: itensRemover });
+  // };
+
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <thead>
@@ -35,8 +44,48 @@ class Table extends Component {
             </th>
           </tr>
         </thead>
+        <tbody>
+          {
+            expenses.map((e) => (
+              <tr key={ e.id }>
+                <td>{e.description}</td>
+                <td>{e.tag}</td>
+                <td>{e.method}</td>
+                <td>{Number(e.value).toFixed(2)}</td>
+                <td>{e.exchangeRates[e.currency].name}</td>
+                <td>{Number(e.exchangeRates[e.currency].ask).toFixed(2)}</td>
+                <td>{Number(e.value * e.exchangeRates[e.currency].ask).toFixed(2)}</td>
+                <td>Real</td>
+                <button
+                  data-testid="edit-btn"
+                  type="button"
+                >
+                  Editar
+
+                </button>
+                <button
+                  data-testid="delete-btn"
+                  type="button"
+                // botãoRemover={ this.botãoRemover }
+                >
+                  Excluir
+
+                </button>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     );
   }
 }
-export default Table;
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
